571dd91beab825a2fe39a32b3f87c99d6e2d5e0c
Andres Sanchez <jugernaru2003@gmail.com>
2024-11-20 16:51:49 -0500

    parte 3

--- app/src/main/java/com/example/dam_uc3_sanchez_tucta/MainActivity.java
+++ app/src/main/java/com/example/dam_uc3_sanchez_tucta/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.dam_uc3_sanchez_tucta;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.btn_buscar_vuelos);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Se enviaron los datos",Toast.LENGTH_SHORT).show();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
